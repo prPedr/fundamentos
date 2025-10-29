1e892ba0e2e8bc650de398e13b5147934a4c6df2
prPedr <prpedro025@gmail.com>
2025-10-29 05:09:18 -0300

    Introducao aos dicionarios

---
+++ dicionarios/dicionarios.py
@@ -0,0 +1,8 @@
+dicionario_pessoa = {
+    "nome": "Pedro",
+    "idade": 20,
+    "estado": "MG",
+    "cidade": "Juiz de Fora"
+}
+
+print(f"{dicionario_pessoa['nome']} tem {dicionario_pessoa['idade']} anos e mora em {dicionario_pessoa['cidade']}-{dicionario_pessoa['estado']}.")
