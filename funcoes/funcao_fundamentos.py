115a3a1a21e5ace708a5ace8c0102d8203873e5a
prPedr <prpedro025@gmail.com>
2025-10-30 04:13:28 -0300

    Aprendendo sobre funcoes

---
+++ funcoes/funcao_fundamentos.py
@@ -0,0 +1,11 @@
+def saudar(nome):
+    print(f"Ola {nome}, seja bem-vindo!")
+
+def multiplicacao(numero1):
+    resultado = numero1 * 10
+    print(f"O resultado da multiplicação é: {resultado}")
+
+def boas_vindas(nome, cidade):
+    print(f"{nome} seja bem-vindo(a) a {cidade}!")
+
+boas_vindas("Pedro", "Juiz de Fora")
