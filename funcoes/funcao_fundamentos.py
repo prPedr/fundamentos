ab05f8a4f5ce58df00c9599cf689a1f7a1c135fc
prPedr <prpedro025@gmail.com>
2025-10-30 04:26:41 -0300

    Trabalhanco com funcoes

--- funcoes/funcao_fundamentos.py
+++ funcoes/funcao_fundamentos.py
@@ -8,4 +8,15 @@ def multiplicacao(numero1):
 def boas_vindas(nome, cidade):
     print(f"{nome} seja bem-vindo(a) a {cidade}!")
 
-boas_vindas("Pedro", "Juiz de Fora")
+def area_retangulo():
+    valor_largura = input("Digite a largura do retângulo: ")
+    valor_altura = input("Digite a altura do retângulo: ")
+
+    valor_altura_float = float(valor_altura)
+    valor_largura_float = float(valor_largura)
+
+    area = valor_altura_float * valor_largura_float
+
+    print(f"A área do retângulo é: {area}")
+
+area_retangulo()
