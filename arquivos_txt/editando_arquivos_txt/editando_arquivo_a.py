676999320dd988ad7f8574fd3cd2d5eda21ef11f
prPedr <prpedro025@gmail.com>
2025-11-03 04:29:14 -0300

    Escrevendo em arquivos utilizando os metodos "a" e "w"

---
+++ arquivos_txt/editando_arquivos_txt/editando_arquivo_a.py
@@ -0,0 +1,2 @@
+with open("teste_escrita_a.txt", "a") as arquivo_a:
+    arquivo_a.write("Teste de sobreescrever uma linha\n")
---
+++ arquivos_txt/editando_arquivos_txt/editando_arquivo_w.py
@@ -0,0 +1,3 @@
+with open("teste_escrita_w.txt", "w") as arquivo_w:
+    arquivo_w.write("Sobreescrevendo meu arquivo\n")
+    arquivo_w.write("Segunda linha de teste\n")
