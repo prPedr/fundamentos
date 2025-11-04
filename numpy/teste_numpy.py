1613f250d4d995be132406125510e851b21b4a2e
prPedr <prpedro025@gmail.com>
2025-11-04 05:34:08 -0300

    Introducao ao numpy

---
+++ numpy/teste_numpy.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+minha_lista = [1, 2, 3]
+
+minha_matriz = [[1, 2, 3], [4, 5, 6]]
+
+print(np.array(minha_lista))
+
+print(sum(minha_lista))
+
+print()
+
+print(np.array(minha_matriz))
+
+print()
+
+print(np.sum(minha_matriz))
+
+print()
+
+print(np.arange(0, 10))
