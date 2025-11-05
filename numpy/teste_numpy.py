3a30aaf3a5e586360a649900aafafd662cc66cc8
prPedr <prpedro025@gmail.com>
2025-11-05 03:49:55 -0300

    Testando metodos em Numpy

--- numpy/teste_numpy.py
+++ numpy/teste_numpy.py
@@ -19,3 +19,35 @@ print(np.sum(minha_matriz))
 print()
 
 print(np.arange(0, 10))
+
+print()
+
+print(np.zeros(3))
+
+print(np.zeros([2, 4]))
+
+print()
+
+print(np.ones(3))
+
+print(np.ones([3, 5]))
+
+print()
+
+print(np.eye(4))
+
+print()
+
+print(np.linspace(0, 100, 50))
+
+print()
+
+print(np.random.random(2)) # Gera uma lista de numeros aleatotios
+
+print()
+
+lista_teste = np.zeros(8)
+
+lista_teste.shape
+
+print(lista_teste.reshape([2, 4]))
